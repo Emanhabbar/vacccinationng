96f3dca57c912f6dec4aebb973dbdeb9a3951327
emanhabbar <emanhabbar22@gmail.com>
2024-11-19 17:55:52 +0300

    update on css and html and create file javascript

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const menuIcon = document.querySelector(".header_icon_box_menu");
+const headerMenu = document.querySelector(".header-menu");
+
+
+menuIcon.addEventListener("click", function () {
+  
+    headerMenu.classList.toggle("visible");
+
+
+});
+
